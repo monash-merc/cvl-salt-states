9c01a67562b3ac66536932c09df56790f4536b08
Chris Hines <chris.hines@monash.edu.au>
2014-08-01 04:17:12 +0000

    home directories shared only to localhost since everything is done over ssh tunnels

--- salt/share_home/exports.py
+++ salt/share_home/exports.py
@@ -1,10 +1,8 @@
 def run():
     clients = salt['mine.get']('roles:desktop','network.ip_addrs',expr_form='grain').items()
-    ipaddrs=[]
-    for c in clients:
-        ipaddrs.append(c[1][0])
+    ipaddrs=['127.0.0.1']
     contents="/mnt/home "
-    opts="(rw,no_root_squash,fsid=0,sync)"
+    opts="(rw,no_root_squash,fsid=0,sync,insecure,insecure_lock)"
     for i in ipaddrs:
         contents=contents+" %s%s"%(i,opts)
     return contents
